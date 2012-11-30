d61b2fedf7b018f8d11782b784c2bd5e3803c439
Lukas Sembera <semberal@gmail.com>
2012-11-30 18:15:11 +0100

    Additional homepage changes

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -2,13 +2,16 @@ package controllers
 
 import play.api.mvc._
 import models.Book
+import play.api.Play
+import play.api.Play.current
 
 object IndexController extends Controller {
 
   def index = Action { implicit request =>
     val books = Book.getBooks()
 
-    Ok(views.html.index(books.take(4)))
+    val bookCount = Play.configuration.getInt("application.homepageBookCount").get
+    Ok(views.html.index(books.take(bookCount)))
   }
 
 }
