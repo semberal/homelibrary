884df07aaeb346d8667bc57c94cb6847c850863e
Lukas Sembera <semberal@gmail.com>
2014-01-12 16:35:30 +0100

    Migrated to Play 2.2

--- app/controllers/ControllerSupport.scala
+++ app/controllers/ControllerSupport.scala
@@ -2,23 +2,27 @@ package controllers
 
 import play.api.mvc._
 import Results._
+import scala.concurrent.Future
+import play.api.libs.concurrent.Akka
 
 trait ControllerSupport {
 
-  def CustomNotFound(implicit requestHeader: RequestHeader) = {
-    NotFound(views.html.notFound())
-  }
+  def CustomNotFound(implicit requestHeader: RequestHeader): SimpleResult = NotFound(views.html.notFound())
 
   def refererRedirect()(implicit request: Request[_]) = {
     Redirect(request.headers.get("Referer").getOrElse("/"))
   }
 
-  def AuthAction(f: Request[AnyContent] => Result): Action[AnyContent] = {
-    Action { request =>
+  object Authenticated extends ActionBuilder[Request] {
+    protected def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[SimpleResult]): Future[SimpleResult] = {
+      import play.api.Play.current
+      implicit val dispatcher = Akka.system.dispatcher
       if (request.session.get("authenticated").isDefined)
-        f(request)
+        block(request)
       else
-        Redirect(routes.IndexController.index).flashing("level" ->"error", "msg" -> "You are not authorized, please log in")
+        Future {
+          Redirect(routes.IndexController.index).flashing("level" -> "error", "msg" -> "You are not authorized, please log in")
+        }
     }
   }
 }
