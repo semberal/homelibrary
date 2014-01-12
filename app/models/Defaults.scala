522bb062f43973af8e4714bba9139b11b7ffd0ee
Lukas Sembera <semberal@gmail.com>
2014-01-12 16:08:41 +0100

    Migrated to Play 2.1

--- app/models/Defaults.scala
+++ app/models/Defaults.scala
@@ -6,9 +6,6 @@ import anorm.{NotAssigned, Id, Pk}
  * @author Lukas Sembera <semberal@gmail.com>
  */
 object Defaults {
-  val GoogleBooksDateFormat = "yyyy-MM-dd"
-  val GoogleBooksShortDateFormat = "yyyy"
-
   /*
    * Implicit conversion from Option[T] to Pk[T] (because of the annoying executeInsert() returning Option[Long]
    */
--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,5 @@
 import sbt._
-import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -8,11 +7,10 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
-	  "com.h2database" % "h2" % "1.3.168"
+      jdbc, anorm, "com.h2database" % "h2" % "1.3.168"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here      
     )
 
